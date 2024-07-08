e5b1687fa63e512205e01bb2823bc10c3ee01e23
Sergio Oseko <gustavosecardovelli@gmail.com>
2024-07-08 14:40:44 -0300

    config: add gunicorn

--- project_1_api/project_1_api/settings.py
+++ project_1_api/project_1_api/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'if0_36862670_project_db',
         'USER': 'if0_36862670',
         'PASSWORD': 'cJ39Un2UgVD3rV9',
-        'HOST': 'sql305.infinityfree.com', 
+        'HOST': 'sql305.infinity.com', 
         'PORT': '3306', 
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
