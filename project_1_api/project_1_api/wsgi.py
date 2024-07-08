b240649051ed2f129210df7ff65debf807387743
Sergio Oseko <gustavosecardovelli@gmail.com>
2024-07-08 16:00:20 -0300

    fix: wsgi

--- project_1_api/project_1_api/wsgi.py
+++ project_1_api/project_1_api/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_1_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_1_api.project_1_api.settings')
 
 application = get_wsgi_application()
